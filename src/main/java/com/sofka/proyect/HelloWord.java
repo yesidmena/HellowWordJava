a1eae10653293618b432a123ffd8ba161270bc25
yesidmena <yesidmenaperosnal@gmail.com>
2022-04-13 21:36:28 -0500

    Se agrega la explicacion del Scanner

--- src/main/java/com/sofka/proyect/HelloWord.java
+++ src/main/java/com/sofka/proyect/HelloWord.java
@@ -1,7 +1,17 @@
 package com.sofka.proyect;
 
+import java.util.Scanner;
+
 public class HelloWord {
     public static void main(String[] args) {
         System.out.println("Hello Word");
+        /**
+         * Scanner se compone de (Scanner nombreDapturador = new Scanner (System.in))
+         *
+         */
+        Scanner captura = new Scanner(System.in);
+        System.out.println("Ingresa un dato");
+        String dato = captura.nextLine();
+        System.out.println(dato);
     }
 }
